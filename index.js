fdedd5c8e352c68463f80b9f5d4c2dce24e6a9a5
Adriana Valdiglesias <adrianavaldiglesias2@gmail.com>
2021-01-13 20:43:18 -0500

    fixed bug issues

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const writeFileAsync = util.promisify(writeToFile);
 const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
 const promptUser = () => {
@@ -18,24 +20,12 @@ const promptUser = () => {
         type: 'input'
     },
     {
-        name: "license",
-        message: "Please select a license.",
-        type: 'list',
-        choices: [
-                "MIT",
-                "Mozilla 2.0",
-                "Boost Software 1.0", 
-                 "Apache 2.0",
-                 "GNU AGPLv3", 
-                 "GNU GPLv3",]
+        name: "installations",
+        message: "installation instructions:",
+        type: 'Input',
     },
     {
-        name: "test",
-        message: "What are the test instructions?",
-        type: "input",
-    },
-    {
-        name: "contributing",
+        name: "contribution",
         message: "Who contributed to your project?",
         type: 'input',
     },
@@ -43,6 +33,11 @@ const promptUser = () => {
         name: "tests",
         message: "tests/instructions",
         type: 'input', 
+    },
+    {
+        name: "usage",
+        message: 'usage commands / instructions:',
+        type: 'input',
     },
     {
         name: "username",
@@ -55,27 +50,45 @@ const promptUser = () => {
         message: "email address",
         type: 'input',
     },
-]);
-
-// TODO: Create a function to write README file
+    {
+        name: "license",
+        message: "Please select a license.",
+        type: 'list',
+        choices: [
+                "None",
+                "MIT",
+                "Mozilla 2.0",
+                "Boost Software 1.0", 
+                 "Apache 2.0",
+                 "GNU AGPLv3", 
+                 "GNU GPLv3",]
+    },
+])};
+//function to initialize app
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(fileName, data);
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success! README.md file has been generated.")
+    })
 }
 
-// TODO: Create a function to initialize app
-const init = async () => {
-    console.log('Generate README file by answering the following questions.')
-    try {
-        const answers = await promptUser();
+async function init() {
+     try {
+        const answers = await inquirer.prompt(questions);
+        console.log("Your response: ", answers);
+       
+        const readme = generateMarkdown(answers);
 
-        const md = generateMarkdown(answers);
-        
-        await fs.writeFileAsync("README.md", md)
-        console.log("Success!");
-    } catch(err) {
+        await writeFileAsync("README.md", readme);
+        console.log("Succesfully written README.md");
+
+    } catch (err) {
         console.log(err);
     }
-};
-
+}
 // Function call to initialize app
-init()};
+init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,26 +9,85 @@ function renderLicenseBadge(license) {
     return  `<a href="https://img.shields.io/badge/License-MIT-yellowgreen?style=plastic"><img alt="MIT License used" src="https://img.shields.io/badge/License-MIT-yellowgreen?style=plastic"></a>`; 
   } else if (license === "Mozilla 2.0") {
     return `<a href="https://img.shields.io/badge/License-Mozilla2.0-yellowgreen?style=plastic"><img alt="Mozilla 2.0 License used" src="https://img.shields.io/badge/License-Mozilla2.0-yellowgreen?style=plastic"></a>`;
-  } else if (license === "")
+  } else if (license === "GNU AGPLv3") {
+    return `<a href="https://img.shields.io/badge/License-GNUAGPLv3-yellowgreen?style=plastic"><img alt="GNU AGPLv3 License used" src="https://img.shields.io/badge/License-GNUAGPLv3-yellowgreen?style=plastic"></a>`;
+  } else if (license === "GNU GPLv3") {
+    return `<a href="https://img.shields.io/badge/License-GNUGPLv3-yellowgreen?style=plastic"><img alt="GNU GPLv3 License used" src="https://img.shields.io/badge/License-GNU GPLv3-yellowgreen?style=plastic"></a>`;
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return ``;
+  } else if (license === "Apache 2.0") {
+    return `http://www.apache.org/licenses/LICENSE-2.0`;
+  } else if (license === "MIT") {
+    return `https://choosealicense.com/licenses/mit/`;
+  } else if (license === "Mozilla 2.0") {
+    return `https://choosealicense.com/licenses/mpl-2.0/`;
+  } else if (license === "GNU AGPLv3") {
+    return `https://choosealicense.com/licenses/agpl-3.0/`;
+  } else if (license === "GNU GPLv3") {
+    return `https://choosealicense.com/licenses/gpl-3.0/`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, name) {
+  if (license === "None") {
+    return ``;
+  } else {
+    return `##License
+    Copyright (c) 2021 ${name}.
+    This project is licensed under [${license}](${renderLicenseLink(license)}) `;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(response) {
+  let response;
   return `# ${response.title}
 
-  ## Description
-  ${response.description}
+  #${response.title}
 
-  ## License
-  ${response.license}
+  #Table of Content
+  -[Description] (#description)
+
+  -[Installation] (#installation)
+  
+  -[Contribution] (#contribution)
+  
+  -[Test] (#test)
+  
+  -[Usage] (#usage)
+  
+  -[Username] (#username)
+  
+  -[License) (#license)
+ 
+    ## Description
+  ${data.description}
+
+    ##Instillation
+    ${data.instillation}
+
+    ##Contribution
+    ${data.contribution}
+
+    ##Test
+    ${data.test}
+
+    ##Usage
+    ${data.usage}
+
+    ##Username
+    ${data.username}
+
+    ## License
+    ${data.license}
 `;
 }
 
